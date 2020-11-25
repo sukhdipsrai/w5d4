253b0e590fa9c947122342968cdabe2e3f6e0a9b
Sukhdip Rai <sukhdipsrai@gmail.com>
2020-11-25 14:42:35 -0500

    some progress made on associations

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+    has_many :enrollment
+    has_many :student
 end
--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    has_many :registrations
+        primary_key: :name
+        foreign_key: :student_id
+        class_name: :enrollment
+    
+    has_many :course
+
+    # house.rb -> id
+    # has_many( :residents, {
+    #     primary_key: :id,   # The primary key for House
+    #     foreign_key: :house_id, # The foreign key that Person contains
+    #     class_name: :Person # The class to instantiate.
+    # } )
+
 end
